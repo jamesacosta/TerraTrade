7ff4d3166cabc1ac5679e088c0bf578c736baf12
Brahian Barrios <brahian.barrios@globant.com>
2024-06-14 20:14:54 -0500

    Header fixed

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -1,10 +1,20 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    var dropdownBtn = document.querySelector('.dropdown-btn');
-    var dropdownContent = document.querySelector('.dropdown-content');
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownContent = document.querySelector('.dropdown-content');
 
     dropdownBtn.addEventListener('click', function() {
         dropdownContent.classList.toggle('show');
+    });
+
+    const links = document.querySelectorAll('.link');
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            document.querySelector(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 });
