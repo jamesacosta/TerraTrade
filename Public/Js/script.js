62a1316aba72f3f93b580b153991dad8265d2a60
kevin <kevindanielm2004@gmail.com>
2024-06-14 22:04:01 -0500

    Carrusel

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -8,3 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
         dropdownContent.classList.toggle('show');
     });
 });
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Change image every 2 seconds
+setInterval(function(){ plusSlides(1) }, 2000);
